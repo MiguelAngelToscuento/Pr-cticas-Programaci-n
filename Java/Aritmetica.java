fefb16184eb7727e1e240ef03dab48e2d1c59880
Miguel Angel Toscuento Flores <miguelangeltoscuentoflores@gmail.com>
2025-09-20 02:26:06 -0600

    Inyeccion de comentarios

--- Java/Aritmetica.java
+++ Java/Aritmetica.java
@@ -1,29 +1,30 @@
 package Java;
 import java.util.Scanner;
 
-public class Aritmetica{
-    public static void main(String[] args){
+public class Aritmetica {
+    public static void main(String[] args) {
+        // mensaje principal del tema
         System.out.println("Aritmetica");
-        //declaracion de objetos y variables
-        Scanner input = new Scanner(System.in);
-        int n1, n2, suma, mult, div, resta, modulo = 0;
-        //solicitud y lectura de datos por teclado
+        // declaracion de objetos y variables
+        Scanner input = new Scanner(System.in); // objeto (input) de la clase Scanner
+        float n1, n2, suma, mult, div, resta, modulo = 0;// variables de operacion
+        // solicitud y lectura de datos por teclado
         System.out.println("Ingresa dos numeros distintos: ");
         n1 = input.nextInt();
         n2 = input.nextInt();
-        //modulo de operaiones
+        // modulo de operaiones
         suma = n1 + n2;
         mult = n1 * n2;
         div = n1 / n2;
         resta = n1 - n2;
         modulo = n1 % n2;
-        //resultados de operaciones
-        System.out.println("La suma es: "+suma);
-        System.out.println("La multiplicacion es: "+mult);
-        System.out.println("La division es: "+div);
-        System.out.println("La resta es: "+resta);
-        System.out.println("El modulo es: "+modulo);
-        //cierre de lectura de datos para evitar ingresar datos erroneos
+        // resultados de operaciones
+        System.out.println("La suma es: " + suma);
+        System.out.println("La multiplicacion es: " + mult);
+        System.out.println("La division es: " + div);
+        System.out.println("La resta es: " + resta);
+        System.out.println("El modulo es: " + modulo);
+        // cierre de lectura de datos para evitar ingresar datos erroneos
         input.close();
     }
 }
